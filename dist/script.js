a87a751d4d1b765667ba450d61a1e8c1f0108db9
rockethyperlocal <mapmydestination@gmail.com>
2020-06-14 21:44:53 +0530

    update

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,9 @@ dragula([
 	document.getElementById('2'),
 	document.getElementById('3'),
 	document.getElementById('4'),
-	document.getElementById('5')
+	document.getElementById('5'),
+	document.getElementById('6'),
+	document.getElementById('7')
 ])
 
 .on('drag', function(el) {
